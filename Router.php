5a1455fda4b240aa8306ce9d18fe0b0eabc73bac
Borja Florin Fernandez <borjasff3@gmail.com>
2023-07-27 20:00:12 +0200

    actualizacion de comprobarRutas

--- Router.php
+++ Router.php
@@ -6,8 +6,8 @@ use GuzzleHttp\Psr7\Header;
 
 class Router {
 
-        public $rutasGET = [];
-        public $rutasPOST = [];
+        public array $rutasGET = [];
+        public array $rutasPOST = [];
 
         public function get($url, $fn){
             $this->rutasGET[$url] = $fn;
@@ -19,14 +19,6 @@ class Router {
 
         public function comprobarRutas(){
 
-            session_start();
-
-            $auth = $_SESSION['login'] ?? null;
-            
-
-            //arreglo de rutas protegidas
-            $rutas_protegidas = ['/admin', 'propiedades/crear', 'propiedades/actualizar', 'propiedades/eliminar', 'vendedores/crear', 'vendedores/actualizar', 'vendedores/eliminar'];
-
             //Detecta con strtok lo que no es esencial de la url como un token y lo elimina
             $urlActual = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
             $metodo = $_SERVER['REQUEST_METHOD'];
@@ -35,11 +27,6 @@ class Router {
                 $fn = $this->rutasGET[$urlActual] ?? null;
             } else {
                 $fn = $this->rutasPOST[$urlActual] ?? null;
-            }
-
-            //proteger ruta
-            if(in_array($urlActual, $rutas_protegidas) && !$auth){
-                header('Location: /');
             }
 
             if($fn){
@@ -52,15 +39,14 @@ class Router {
 
         //muestra una vista que le pasamos
         public function render($view, $datos = [] ){
-            
             foreach($datos as $key => $value){
                 $$key = $value;
             }
 
             ob_start();//alamacenamiento en memoria temporal
 
-            include __DIR__ . "/views/$view.php";
+            include_once __DIR__ . "/views/$view.php";
             $contenido = ob_get_clean(); //limpia el buffer
-            include __DIR__ . "/views/layout.php";
+            include_once __DIR__ . "/views/layout.php";
         }
 }
