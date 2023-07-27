89531012ae22a6e53066d502e14f08de1a59cd01
Borja Florin Fernandez <borjasff3@gmail.com>
2023-07-27 18:51:31 +0200

    arreglo login

---
+++ OneDrive/Escritorio/BIENESRAICES_MVC_PHP_BFF_V1/views/layout.php
@@ -0,0 +1,72 @@
+<?php
+    
+    if(!isset($_SESSION)){
+    session_start();
+    }
+    //var_dump($_SESSION);
+
+    $auth = $_SESSION['login'] ?? false;
+
+    if(!isset($inicio)){
+        $inicio = false;
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienes Raices</title>
+    <link rel="stylesheet" href="/build/css/app.css">
+</head>
+<body>
+    <header class="header <?php echo $inicio ? 'inicio': ''; ?>">
+        <div class="contenedor contenido-header">
+            <div class="barra">
+                <a href="/">
+                    <img class="logo-header" src="/build/img/logo.svg" alt="Logotipo de Bienes Raices">
+                </a>
+                <div class="mobile-menu">
+                    <img src="/build/img/barras.svg" alt="menu responsive">
+                </div>
+                <div class="derecha">
+                    <img class="dark-mode-boton" src="/build/img/dark-mode.svg">
+                <nav class="navegacion">
+                    <a href="/nosotros">Nosotros</a>
+                    <a href="/propiedades">Anuncios</a>
+                    <a href="/blog">Blog</a>
+                    <a href="/contacto">Contacto</a>
+                <?php if (!$auth) : ?>
+                <a href="/login">Iniciar Sesion</a>
+                <?php endif; ?>
+                <?php if ($auth) : ?>
+                <a href="/logout">Cerrar Sesion</a>
+                <?php endif; ?>
+
+                </nav>
+                </div>
+
+            </div><!--Fin barra navegadora-->
+            <?php echo $inicio ? "<h1>Venta de Casas y Departamentos Exclusivos de Lujo</h1>" : '';?>
+
+        </div>
+    </header>
+
+    <?php echo $contenido; ?>
+
+    <footer class="footer seccion">
+        <div class="contenedor contenedor-footer">
+                <nav class="navegacion">
+                    <a href="/nosotros">Nosotros</a>
+                    <a href="/propiedades">Anuncios</a>
+                    <a href="/blog">Blog</a>
+                    <a href="/contacto">Contacto</a>
+                </nav>
+        </div>
+        <p class="copyright">Todos los derechos reservados <?php echo date('Y'); ?> &copy;</p>
+    </footer>
+
+    <script src="../build/js/bundle.min.js"></script>
+</body>
+</html>
